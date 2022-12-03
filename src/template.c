b95620a98154535978f9665ca3388a09403958ef
Tobias Harvey <tobias.harvey@tobias-harvey.local>
2022-12-03 11:39:24 -0700

    added a template?

---
+++ src/template.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void p1()
+{
+
+  FILE *fin = fopen("../inputs/day<>", "r");
+
+  
+  fclose(fin);
+  
+}
+
+void p2()
+{
+
+  FILE *fin = fopen("../inputs/day<>", "r");
+
+  
+  fclose(fin);
+
+}
+
+int main(){
+
+  p1();
+  p2();
+
+}
